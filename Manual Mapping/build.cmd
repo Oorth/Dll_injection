1b7e163fa1e28575a4f4597f0634c5e489f98b8d
Oorth <arthmaurya@gmail.com>
2025-05-17 21:57:46 +0530

    automated the process

--- Manual Mapping/build.cmd
+++ Manual Mapping/build.cmd
@@ -1,8 +1,30 @@
+@REM @REM cl /EHsc /GS- /Oy /Zi .\main.cpp .\injection.cpp /link /OUT:main.exe /DEBUG /MAP
+@REM @REM cl /EHsc /GS- /Oy /Zi .\main.cpp .\injection.cpp /link /OUT:main.exe /DEBUG /MAP /DEF:main.def
+@REM @REM cl /EHsc /GS- /Oy /Zi .\main.cpp .\injection.cpp /link /OUT:main.exe /VERBOSE /DEBUG /MAP
+@REM @REM cl /c /EHsc /GS- /Oy /Zi /FAs .\injection.cpp
+
+
+@echo off
 cls
 del main.pdb main.map main.ilk injection.obj main.obj main.exe injection.asm main.lib main.exp vc140.pdb 2>nul
 cls
+
+@REM /wait
 cl /EHsc /GS- /Oy /Zi .\main.cpp .\injection.cpp /link /OUT:main.exe /DEBUG /MAP /INCREMENTAL:NO
-@REM cl /EHsc /GS- /Oy /Zi .\main.cpp .\injection.cpp /link /OUT:main.exe /DEBUG /MAP
-@REM cl /EHsc /GS- /Oy /Zi .\main.cpp .\injection.cpp /link /OUT:main.exe /DEBUG /MAP /DEF:main.def
-@REM cl /EHsc /GS- /Oy /Zi .\main.cpp .\injection.cpp /link /OUT:main.exe /VERBOSE /DEBUG /MAP
-@REM cl /c /EHsc /GS- /Oy /Zi /FAs .\injection.cpp
+
+if errorlevel 1 (
+    goto :eof
+)
+
+start /min "" notepad.exe
+timeout /t 0 /nobreak >nul
+
+for /f "tokens=2 delims=," %%i in ('tasklist /nh /fi "imagename eq notepad.exe" /fo csv') do @set "notepadPID=%%i"
+if defined notepadPID (
+    start /min "" "C:\tools\x32 & x64 dbg\release\x64\x64dbg.exe" -p %notepadPID%
+) else (
+    echo Error: Could not find notepad.exe's PID.
+)
+
+endlocal
+goto :eof
