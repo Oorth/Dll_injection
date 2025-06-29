d96f2ad81ea5896ea3f07391ba4cb562166235aa
Oorth <arthmaurya@gmail.com>
2025-06-29 23:33:48 +0530

    v2.2.0 [Fixed the Deadlock]

--- basic_dll/cute_lib.cpp
+++ basic_dll/cute_lib.cpp
@@ -1,22 +1,41 @@
 //cl /EHsc /LD .\cute_lib.cpp /link User32.lib
 #include <windows.h>
 
+DWORD WINAPI ShowMessageBoxThread(LPVOID lpParam)
+{
+    const char* message = (const char*)lpParam;
+    MessageBoxA(NULL, message, "Notification", MB_OK);
+    return 0;
+}
+
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved)
 {
+    const char* msg = nullptr;
     switch (ul_reason_for_call)
     {
     case DLL_PROCESS_ATTACH:
-        MessageBoxA(NULL, "DLL Attached", "Notification", MB_OK);
+        msg = "Process Attached";
         break;
     case DLL_THREAD_ATTACH:
-        MessageBoxA(NULL, "Thread Attached", "Notification", MB_OK);
+        msg = "Thread Attached";
         break;
     case DLL_THREAD_DETACH:
-        MessageBoxA(NULL, "Thread Detached", "Notification", MB_OK);
+        msg = "Thread Detached";
         break;
     case DLL_PROCESS_DETACH:
-        MessageBoxA(NULL, "DLL Detached", "Notification", MB_OK);
+        msg = "Process Detached";
         break;
+    }
+    if (msg)
+    {
+        // Duplicate the message string so it's valid in the new thread
+        char* msgCopy = _strdup(msg);
+        if (msgCopy)
+        {
+            HANDLE hThread = CreateThread(NULL, 0, ShowMessageBoxThread, msgCopy, 0, NULL);
+            if (hThread)
+                CloseHandle(hThread);
+        }
     }
     return TRUE;
 }
